13533eae224922f9e929b79b93c06cf788d81da8
arielaleco <arielaleco@gmail.com>
2018-06-19 02:44:25 +0300

    add animations

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 $(function () {
 
-    $("#projects-btn").click(function(){
+   
+
+    $("#projects-btn").click(function () {
+
+        // $("#about-me").hide();
+         $("#middle-panel").css("opacity", "0");
+        // $("#about-me").css("line-height", "2em");
+      
+
         $('html, body').animate({
-            scrollTop: $("#projects").offset().top
-        }, 2000);
+            scrollTop: $("#projects").offset().top}, 2000,function(){       
+            $("#middle-panel").animate({
+                left: '250px',
+                opacity: '0.9',
+                height: '150px',
+                width: '150px'
+            });
+
+        });
+        
     });
+    $('#contact-btn').click(function () {
+        $('html, body').animate({
+            scrollTop: $("footer").offset().top
+        }, 2000);
+
+    });
+    $('#about-btn').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#welcome-section").offset().top
+        }, 2000);
+
+    });
+
 });
 
 
