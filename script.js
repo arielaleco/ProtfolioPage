6a9810e98f1088ce311242f0ef4f66fa78e3b946
arielaleco <arielaleco@gmail.com>
2018-06-19 14:44:46 +0300

    responsive

--- script.js
+++ script.js
@@ -8,35 +8,35 @@ $(function () {
         //  $("#middle-panel").css("opacity", "0");
 
 
-        $("#middle-panel").animate({
-            opacity: '0'
+        // $("#middle-panel").animate({
+        //     opacity: '0'
 
-        });
-        $("#about-me").css("line-height", "0.9em");
-        $("#about-me").css("margin", "0");
+        // });
+        // $("#about-me").css("line-height", "0.9em");
+        // $("#about-me").css("margin", "0");
         $('html, body').animate({
             scrollTop: $("#projects").offset().top
         }, 2000, function () {
-            $("#middle-panel").show(2000);
+           // $("#middle-panel").show(2000);
 
-            $("#trans-box").animate({
-                left: '10%',               
-                height: '650px',
-                width: '10px',
-                top: "120px"
+            // $("#trans-box").animate({
+            //     left: '10%',               
+            //     height: '650px',
+            //     width: '10px',
+            //     top: "120px"
 
-            }, 1000);
+            // }, 1000);
 
 
 
-            $("#middle-panel").animate({
-                left: '250px',
-                opacity: '0.9',
-                height: '650px',
-                width: '180px',
-                top: "400px"
+            // $("#middle-panel").animate({
+            //     left: '250px',
+            //     opacity: '0.9',
+            //     height: '650px',
+            //     width: '180px',
+            //     top: "400px"
 
-            }, 1000);
+            // }, 1000);
 
         });
 
