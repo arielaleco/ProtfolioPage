782ffc25f1c2c2ee70143ecd3448d283c667dd05
arielaleco <arielaleco@gmail.com>
2018-06-19 10:00:47 +0300

    add animations

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 $(function () {
 
-   
+
 
     $("#projects-btn").click(function () {
 
         // $("#about-me").hide();
-         $("#middle-panel").css("opacity", "0");
-        // $("#about-me").css("line-height", "2em");
-      
+        //  $("#middle-panel").css("opacity", "0");
 
+
+        $("#middle-panel").animate({
+            opacity: '0'
+
+        });
+        $("#about-me").css("line-height", "0.9em");
+        $("#about-me").css("margin", "0");
         $('html, body').animate({
-            scrollTop: $("#projects").offset().top}, 2000,function(){       
+            scrollTop: $("#projects").offset().top
+        }, 2000, function () {
+            $("#middle-panel").show(2000);
+
+            $("#trans-box").animate({
+                left: '10%',               
+                height: '650px',
+                width: '10px',
+                top: "120px"
+
+            }, 1000);
+
+
+
             $("#middle-panel").animate({
                 left: '250px',
                 opacity: '0.9',
-                height: '150px',
-                width: '150px'
-            });
+                height: '650px',
+                width: '180px',
+                top: "400px"
+
+            }, 1000);
 
         });
-        
+
     });
     $('#contact-btn').click(function () {
         $('html, body').animate({
