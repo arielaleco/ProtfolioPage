dbeaa4f59def54d4676d23b534c439b3e594f208
arielaleco <arielaleco@gmail.com>
2018-06-19 12:05:50 +0300

    view port is not full yet

--- script.js
+++ script.js
@@ -42,15 +42,39 @@ $(function () {
 
     });
     $('#contact-btn').click(function () {
+  
+       // transform: rotate(-90deg);
+                var div = $(".contacts-info");
+                var c1 = $("#connect-me > .circle:nth-of-type(1) ");
+                var c2 = $("#connect-me > .circle:nth-of-type(2) ");
+                var c3 = $("#connect-me > .circle:nth-of-type(3) ");
+                c1.hide();
+                c2.hide();
+                c3.hide();
+                //div.animate({transform: 'rotate(45deg)',fontSize:"140px"}, "slow");
+                // div.animate({transform: 'rotate(0deg)', opacity: '0.4'}, "slow");
+                
+               
+               
+               //  div.animate({width: '100%', opacity: '0.8'}, "slow");
+                // div.animate({height: '100px', opacity: '0.4'}, "slow");
+                // div.animate({width: '100px', opacity: '0.8'}, "slow");
+            
+
         $('html, body').animate({
             scrollTop: $("footer").offset().top
-        }, 2000);
+        }, 2000  ,function(){
+            c1.fadeIn(500); 
+            c2.fadeIn(1000); 
+            c3.fadeIn(1500); 
+        
+        }   );
 
     });
     $('#about-btn').click(function () {
         $('html, body').animate({
             scrollTop: $("#welcome-section").offset().top
-        }, 2000);
+        }, 1000);
 
     });
 
